8d362be0e9a6c04d7f2a23a2ac652159e8385f7a
rediott <setiadiraihan@gmail.com>
2023-08-11 09:38:28 +0700

    Many stuff added, i forgot

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+
+
+
+const Navbar = () =>{
+
+    return(
+        <>
+        <nav>
+            <h2><Link to="/">YouSoup</Link></h2> 
+        </nav>
+        </>
+    )
+ 
+
+}
+
+export default Navbar;
